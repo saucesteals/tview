8e7341fea568fcb48a90feecf21855fb9703c856
Daniel <saucesteals@gmail.com>
2022-05-18 11:00:35 -0700

    export form & frame

--- modal.go
+++ modal.go
@@ -14,11 +14,11 @@ import (
 type Modal struct {
 	*Box
 
-	// The frame embedded in the modal.
-	frame *Frame
+	// The Frame embedded in the modal.
+	Frame *Frame
 
-	// The form embedded in the modal's frame.
-	form *Form
+	// The Form embedded in the modal's frame.
+	Form *Form
 
 	// The message text (original, not word-wrapped).
 	text string
@@ -37,18 +37,18 @@ func NewModal() *Modal {
 		Box:       NewBox(),
 		textColor: Styles.PrimaryTextColor,
 	}
-	m.form = NewForm().
+	m.Form = NewForm().
 		SetButtonsAlign(AlignCenter).
 		SetButtonBackgroundColor(Styles.PrimitiveBackgroundColor).
 		SetButtonTextColor(Styles.PrimaryTextColor)
-	m.form.SetBackgroundColor(Styles.ContrastBackgroundColor).SetBorderPadding(0, 0, 0, 0)
-	m.form.SetCancelFunc(func() {
+	m.Form.SetBackgroundColor(Styles.ContrastBackgroundColor).SetBorderPadding(0, 0, 0, 0)
+	m.Form.SetCancelFunc(func() {
 		if m.done != nil {
 			m.done(-1, "")
 		}
 	})
-	m.frame = NewFrame(m.form).SetBorders(0, 0, 1, 0, 0, 0)
-	m.frame.SetBorder(true).
+	m.Frame = NewFrame(m.Form).SetBorders(0, 0, 1, 0, 0, 0)
+	m.Frame.SetBorder(true).
 		SetBackgroundColor(Styles.ContrastBackgroundColor).
 		SetBorderPadding(1, 1, 1, 1)
 	return m
@@ -56,20 +56,20 @@ func NewModal() *Modal {
 
 // SetBorderColor sets the color of the border.
 func (m *Modal) SetBorderColor(color tcell.Color) *Modal {
-	m.frame.SetBorderColor(color)
+	m.Frame.SetBorderColor(color)
 	return m
 }
 
 // SetBorder enables or disables the display of the border
 func (m *Modal) SetBorder(show bool) *Modal {
-	m.frame.SetBorder(show)
+	m.Frame.SetBorder(show)
 	return m
 }
 
 // SetBackgroundColor sets the color of the modal frame background.
 func (m *Modal) SetBackgroundColor(color tcell.Color) *Modal {
-	m.form.SetBackgroundColor(color)
-	m.frame.SetBackgroundColor(color)
+	m.Form.SetBackgroundColor(color)
+	m.Frame.SetBackgroundColor(color)
 	return m
 }
 
@@ -81,13 +81,13 @@ func (m *Modal) SetTextColor(color tcell.Color) *Modal {
 
 // SetButtonBackgroundColor sets the background color of the buttons.
 func (m *Modal) SetButtonBackgroundColor(color tcell.Color) *Modal {
-	m.form.SetButtonBackgroundColor(color)
+	m.Form.SetButtonBackgroundColor(color)
 	return m
 }
 
 // SetButtonTextColor sets the color of the button texts.
 func (m *Modal) SetButtonTextColor(color tcell.Color) *Modal {
-	m.form.SetButtonTextColor(color)
+	m.Form.SetButtonTextColor(color)
 	return m
 }
 
@@ -113,12 +113,12 @@ func (m *Modal) SetText(text string) *Modal {
 func (m *Modal) AddButtons(labels []string) *Modal {
 	for index, label := range labels {
 		func(i int, l string) {
-			m.form.AddButton(label, func() {
+			m.Form.AddButton(label, func() {
 				if m.done != nil {
 					m.done(i, l)
 				}
 			})
-			button := m.form.GetButton(m.form.GetButtonCount() - 1)
+			button := m.Form.GetButton(m.Form.GetButtonCount() - 1)
 			button.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 				switch event.Key() {
 				case tcell.KeyDown, tcell.KeyRight:
@@ -135,31 +135,31 @@ func (m *Modal) AddButtons(labels []string) *Modal {
 
 // ClearButtons removes all buttons from the window.
 func (m *Modal) ClearButtons() *Modal {
-	m.form.ClearButtons()
+	m.Form.ClearButtons()
 	return m
 }
 
 // SetFocus shifts the focus to the button with the given index.
 func (m *Modal) SetFocus(index int) *Modal {
-	m.form.SetFocus(index)
+	m.Form.SetFocus(index)
 	return m
 }
 
 // Focus is called when this primitive receives focus.
 func (m *Modal) Focus(delegate func(p Primitive)) {
-	delegate(m.form)
+	delegate(m.Form)
 }
 
 // HasFocus returns whether or not this primitive has focus.
 func (m *Modal) HasFocus() bool {
-	return m.form.HasFocus()
+	return m.Form.HasFocus()
 }
 
 // Draw draws this primitive onto the screen.
 func (m *Modal) Draw(screen tcell.Screen) {
 	// Calculate the width of this modal.
 	buttonsWidth := 0
-	for _, button := range m.form.buttons {
+	for _, button := range m.Form.buttons {
 		buttonsWidth += TaggedStringWidth(button.label) + 4 + 2
 	}
 	buttonsWidth -= 2
@@ -171,7 +171,7 @@ func (m *Modal) Draw(screen tcell.Screen) {
 	// width is now without the box border.
 
 	// Reset the text and find out how wide it is.
-	m.frame.Clear()
+	m.Frame.Clear()
 	var lines []string
 	for _, line := range strings.Split(m.text, "\n") {
 		if len(line) == 0 {
@@ -182,7 +182,7 @@ func (m *Modal) Draw(screen tcell.Screen) {
 	}
 	//lines := WordWrap(m.text, width)
 	for _, line := range lines {
-		m.frame.AddText(line, true, AlignCenter, m.textColor)
+		m.Frame.AddText(line, true, AlignCenter, m.textColor)
 	}
 
 	// Set the modal's position and size.
@@ -193,15 +193,15 @@ func (m *Modal) Draw(screen tcell.Screen) {
 	m.SetRect(x, y, width, height)
 
 	// Draw the frame.
-	m.frame.SetRect(x, y, width, height)
-	m.frame.Draw(screen)
+	m.Frame.SetRect(x, y, width, height)
+	m.Frame.Draw(screen)
 }
 
 // MouseHandler returns the mouse handler for this primitive.
 func (m *Modal) MouseHandler() func(action MouseAction, event *tcell.EventMouse, setFocus func(p Primitive)) (consumed bool, capture Primitive) {
 	return m.WrapMouseHandler(func(action MouseAction, event *tcell.EventMouse, setFocus func(p Primitive)) (consumed bool, capture Primitive) {
 		// Pass mouse events on to the form.
-		consumed, capture = m.form.MouseHandler()(action, event, setFocus)
+		consumed, capture = m.Form.MouseHandler()(action, event, setFocus)
 		if !consumed && action == MouseLeftClick && m.InRect(event.Position()) {
 			setFocus(m)
 			consumed = true
@@ -213,8 +213,8 @@ func (m *Modal) MouseHandler() func(action MouseAction, event *tcell.EventMouse,
 // InputHandler returns the handler for this primitive.
 func (m *Modal) InputHandler() func(event *tcell.EventKey, setFocus func(p Primitive)) {
 	return m.WrapInputHandler(func(event *tcell.EventKey, setFocus func(p Primitive)) {
-		if m.frame.HasFocus() {
-			if handler := m.frame.InputHandler(); handler != nil {
+		if m.Frame.HasFocus() {
+			if handler := m.Frame.InputHandler(); handler != nil {
 				handler(event, setFocus)
 				return
 			}
